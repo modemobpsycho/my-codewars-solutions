0efaef4d89bdd50f2aa5e547e0e98d49930f9ce6
Vadim <hawaiianwork@mail.ru>
2024-01-17 22:32:16 +0300

    2kyu

---
+++ 2kyu/One Line Task Circle Intersection.py
@@ -0,0 +1,10 @@
+from numpy import *
+
+circleIntersection = (
+    lambda a, b, r: r
+    * r
+    * (lambda h: h < 1 and arccos(h) - h * (1 - h * h) ** 0.5)(
+        hypot(*subtract(b, a)) / r / 2
+    )
+    // 0.5
+)
