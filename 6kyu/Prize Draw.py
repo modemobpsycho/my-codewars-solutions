393f9c0e7a2ccee4022ed0a41d848bb7e27fb522
Vadim <hawaiianwork@mail.ru>
2024-02-01 19:31:52 +0300

    6kyu/Prize Draw

---
+++ 6kyu/Prize Draw.py
@@ -0,0 +1,20 @@
+def getWeight(name, weighting):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    weight = 0
+    for letter in name.lower():
+        print(letter, alphabet.index(letter))
+        weight += alphabet.index(letter) + 2
+    return {"name": name, "weight": (weight + len(name)) * weighting}
+
+
+def rank(st, we, n):
+    names = st.split(",")
+    if len(st) == 0:
+        return "No participants"
+    if len(names) < n:
+        return "Not enough participants"
+    weightings = sorted(
+        list(map(getWeight, names, we)),
+        key=lambda score_obj: (-score_obj["weight"], score_obj["name"]),
+    )
+    return weightings[n - 1]["name"]
